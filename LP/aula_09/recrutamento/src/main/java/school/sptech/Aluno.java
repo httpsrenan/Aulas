8b1080b0ba6d7399e67d31a5d8f9308bf4aa9e07
httpsrenan <renan.pereira@sptech.school>
2025-10-07 22:08:20 -0300

    atvd lp

--- LP/aula_09/recrutamento/src/main/java/school/sptech/Aluno.java
+++ LP/aula_09/recrutamento/src/main/java/school/sptech/Aluno.java
@@ -7,9 +7,59 @@ public class Aluno {
     private String nome;
     private Integer ra;
 
-    List<Aluno> notas =  new ArrayList<>();
+    private List<Double> notas =  new ArrayList<>();
+
+    public Aluno(Integer ra, String nome) {
+        this.ra = ra;
+        this.nome = nome;
+    }
 
     public void  adicionarNota(Double nota){
         notas.add(nota);
+    }
+
+    public Double calcularMedia(){
+        Double media = 0.0;
+        for (int i = 0; i<notas.size(); i++){
+            media += notas.get(i);
+        }
+        if(notas.size()>0){
+            media = media/notas.size();
+            return media;
+        } else{
+            return 0.0;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getRa() {
+        return ra;
+    }
+
+    public void setRa(Integer ra) {
+        this.ra = ra;
+    }
+
+    public List<Double> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<Double> notas) {
+        this.notas = notas;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno = " +
+                "nome='" + nome + '\'' +
+                "| ra=" + ra +
+                '}';
     }
 }
--- LP/aula_09/recrutamento/src/main/java/school/sptech/Turma.java
+++ LP/aula_09/recrutamento/src/main/java/school/sptech/Turma.java
@@ -1,4 +1,136 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turma {
+
+    private static List<Aluno> alunos =  new ArrayList<>();
+
+    public void adicionarAluno(Aluno nome){
+        alunos.add(nome);
+        System.out.println("Aluno adicionado com sucesso!");
+    }
+
+    public static String getAlunoPorRa(Integer ra){
+            for (int i = 0; i<alunos.size(); i++){
+                if(ra.equals(alunos.get(i).getRa())){
+                    System.out.println("Aluno encontrado");
+                    System.out.println(alunos.get(i));
+                    return alunos.get(i).getNome();
+                }
+            }
+                System.out.println("Aluno não encontrado");
+                return null;
+    }
+
+    public static List<String> getAlunoPorNome(String nome){
+        List<String> alunosEncontrados = new ArrayList<>();
+            String nomeBusca = nome.toLowerCase();
+
+            for (int i = 0; i < alunos.size(); i++) {
+                if (alunos.get(i).getNome().toLowerCase().contains(nomeBusca)) {
+                    alunosEncontrados.add(alunos.get(i).getNome());
+                }
+            }
+            if(!alunosEncontrados.isEmpty()){
+                System.out.println("Aluno encontrado");
+                return alunosEncontrados;
+            }else {
+                return alunosEncontrados;
+            }
+    }
+
+    public static String getAlunoComMaiorMedia() {
+        if (alunos.isEmpty()) {
+            return null;
+        } else {
+            Double maiorMedia = 0.0;
+            String alunoComMaiorMedia = "";
+            for (int i = 0; i < alunos.size(); i++) {
+                Double mediaAtual = alunos.get(i).calcularMedia();
+                if (mediaAtual > maiorMedia) {
+                    maiorMedia = mediaAtual;
+                    alunoComMaiorMedia = alunos.get(i).getNome();
+                }
+            }
+            return alunoComMaiorMedia;
+        }
+    }
+
+    public static String getAlunoComMenorMedia() {
+        if (alunos.isEmpty()) {
+            return null;
+        } else {
+            Double menorMedia = 100.0;
+            String alunoComMenorMedia = "";
+            for (int i = 0; i < alunos.size(); i++) {
+                Double mediaAtual = alunos.get(i).calcularMedia();
+                if (mediaAtual < menorMedia) {
+                    menorMedia = mediaAtual;
+                    alunoComMenorMedia = alunos.get(i).getNome();
+                }
+            }
+            return alunoComMenorMedia;
+        }
+    }
+
+    public static Double calcularMediaDaTurma(){
+        if (alunos.isEmpty()) {
+            return 0.0;
+        }
+        else {
+            Double mediaAtual = 0.0;
+            for (int i = 0; i < alunos.size(); i++) {
+                mediaAtual += alunos.get(i).calcularMedia();
+            }
+            mediaAtual = mediaAtual / alunos.size();
+            return mediaAtual;
+        }
+    }
+
+    public static Double getMediaPorRa(Integer ra) {
+        for (int i = 0; i < alunos.size(); i++) {
+            if (ra.equals(alunos.get(i).getRa())) {
+                return alunos.get(i).calcularMedia();
+            }
+        }
+        return 0.0;
+    }
+
+    public static Double getMediana() {
+        if (alunos.isEmpty()) {
+            return 0.0;
+        } else {
+            List<Double> medias = new ArrayList<>();
+            for (int i = 0; i < alunos.size(); i++) {
+                medias.add(alunos.get(i).calcularMedia());
+            }
+            int n = medias.size();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 1; j < n - i; j++) {
+
+                    if (medias.get(j - 1) > medias.get(j)) {
+                        Double aux = medias.get(j - 1);
+                        medias.set(j - 1, medias.get(j));
+                        medias.set(j, aux);
+                    }
+                }
+            }
+            if (n % 2 != 0) {
+                int Mediana = (n - 1) / 2;
+                return medias.get(Mediana);
+        }
+            else  {
+                Integer indiceMediana1 = (n - 1) / 2;
+                Integer indiceMediana2 = ((n - 1) / 2 ) + 1;
+
+                Double mediana1 = medias.get(indiceMediana1);
+                Double mediana2 = medias.get(indiceMediana2);
+
+                Double mediana = (mediana1 + mediana2)/2;
+                return mediana;
+            }
+    }
+    }
 }
